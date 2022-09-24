666a89229b6fda2cd103f8251409297fc5d15264
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-24 13:04:10 +0900

    hspec-discover

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,7 @@
 module Lib where
 
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello World!"
+
 plus :: Integer -> Integer -> Integer
 plus = ( + )
---
+++ test/MyLibSpec.hs
@@ -0,0 +1,9 @@
+module MyLibSpec where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+import qualified Lib
+
+spec :: Spec
+spec = describe "plus" $ do
+  it "1 plus 1 should be 2" $ do
+    Lib.plus 1 1 `shouldBe` 2
